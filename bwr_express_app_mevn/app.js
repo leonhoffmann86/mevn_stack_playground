b5a0d9739becc7093d4bdfcd99d8ed9155748bf0
Leon Hoffmann <leon.hoffmann@beawwwer.de>
2020-04-11 08:42:47 +0200

    setting up db

--- bwr_express_app_mevn/app.js
+++ bwr_express_app_mevn/app.js
@@ -4,23 +4,28 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var app = express();
+
+// Require file system module
+var fs = require("file-system");
+
+// Include controllers
+fs.readdirSync("controllers").forEach(function (file) {
+  if (file.substr(-3) == ".js") {
+    const route = require("./controllers/" + file);
+    route.controller(app);
+  }
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -38,8 +43,8 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen(3000, function () {
-  console.log("listening on 3000");
+app.listen(3001, function () {
+  console.log("listening on 3001");
 });
 
 module.exports = app;
